66dc5d44a1ddcde2d152437fcfc1d08e5ba4c8db
Albert Wang <awan959@cse.unsw.edu.au>
2013-09-17 19:49:45 +1000

    Initial Project commit. Amazezon project added. Barebones atm

---
+++ Amazezon/src/main/controller/HeaderFilter.java
@@ -0,0 +1,60 @@
+package main.controller;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class HeaderFilter
+ */
+@WebFilter(dispatcherTypes = {
+				DispatcherType.REQUEST, 
+				DispatcherType.FORWARD
+		}
+					, urlPatterns = { "*.jsp" })
+public class HeaderFilter implements Filter {
+	final Logger logger = Logger.getLogger(this.getClass().getName());
+
+    /**
+     * Default constructor. 
+     */
+    public HeaderFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		logger.info("HeaderFilter invoked");
+		chain.doFilter(request, response);
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate"); 
+		res.setHeader("Pragma", "no-cache"); 
+		res.setDateHeader("Expires", 0);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
