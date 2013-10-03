e08e43c77fff0c7b8bc687ab230156b041e36ef5
Albert Wang <awan959@cse.unsw.edu.au>
2013-10-04 01:13:17 +1000

    Added user class

--- Amazezon/src/main/model/data/User.java
+++ Amazezon/src/main/model/data/User.java
@@ -1,5 +1,12 @@
 package main.model.data;
 
 public class User {
-	
+	private int userID;
+	private String username;
+	private String firstname;
+	private String lastname;
+	private String address;
+	private String creditCard;
+	private String email;
+	private int dob;
 }
