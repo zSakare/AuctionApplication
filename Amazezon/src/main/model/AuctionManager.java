d31a7de136248cf708207663ae9805112b71db14
Albert Wang <awan959@cse.unsw.edu.au>
2013-10-05 23:53:41 +1000

    Lower than reserve price wins are now serviced

--- Amazezon/src/main/model/AuctionManager.java
+++ Amazezon/src/main/model/AuctionManager.java
@@ -54,6 +54,13 @@ public class AuctionManager {
 						html = "Your auction for: " + auction.getTitle() + " has finished.";
 						sender.sendMessage("rofllol@gmail.com", auction.getOwner().getEmail(),
 								"Auction finished", html);
+					} else {
+						MailSender sender = MailSender.getMailSender();
+						String html = "Your auction for: " + auction.getTitle() + " has finished but the winning bid is lower than the reserved price." +
+								" Click <a href='http://localhost:8080/Amazezon/accept=" + auctionID + "'>Accept</a> to accept the winning bid." +
+								"Or click <a href='http://localhost:8080/Amazezon/reject=" + auctionID + "'>Reject</a> to reject the winning bid.";
+						sender.sendMessage("rofllol@gmail.com", auction.getOwner().getEmail(),
+								"Auction finished", html);
 					}
 				}
 			} catch (InterruptedException e) {
